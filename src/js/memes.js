cf23f91fbef2a5181a15ffcfbb754a52ef2a02c1
wonderboy <betti.francesco@gmail.com>
2022-05-17 20:30:48 +0200

    implement text rendering and download

--- src/js/memes.js
+++ src/js/memes.js
@@ -21,7 +21,8 @@ class Memes {
 		this.createMeme = this.createMeme.bind(this);
 		let inputNodes = [this.$topTextInput, this.$bottomTextInput, this.$imageInput];
 		// inputNodes.forEach(element => element.addEventListener('keyup', this.createMeme));
-		inputNodes.forEach(element => element.addEventListener('change', this.createMeme));
+		inputNodes.forEach(element => element.addEventListener('change', this.createMeme.bind(this)));
+		this.$downloadButton.addEventListener('click', this.downloadMeme.bind(this));
 	}
 
 	createCanvas() {
@@ -33,6 +34,7 @@ class Memes {
 
 	createMeme() {
 		let context = this.$canvas.getContext('2d');
+		console.log(`canvas context ${context}`);
 		if (this.$imageInput.files && this.$imageInput.files[0]) {
 			let reader = new FileReader();
 
@@ -45,17 +47,72 @@ class Memes {
 					context.clearRect(0, 0, this.$canvas.height, this.$canvas.width);
 					context.drawImage(image, 0, 0);
 
-
-					let fontsize = ((this.$canvas.height + this.$canvas.width) / 2.0) * 4.0 / 100.0;
-					context.font = `${fontsize}pt sans-serif`;
+					let fontSize = ((this.$canvas.width + this.$canvas.height) / 2) * 4 / 100;
+					context.font = `${fontSize}pt sans-serif`;
 					context.textAlign = 'center';
-					context.textBaseLine = 'top';
+					context.textBaseline = 'top';
+
+					// for stroke text
+					context.lineWidth = fontSize / 5;
+					context.strokeStyle = 'black';
+
+					// for fill text
+					context.fillStyle = 'white';
+					context.lineJoin = 'round';
+
+					const topText = this.$topTextInput.value.toUpperCase();
+					const bottomText = this.$bottomTextInput.value.toUpperCase();
+
+					// Top Text
+					context.strokeText(topText, this.$canvas.width / 2, this.$canvas.height * (5 / 100));
+					context.fillText(topText, this.$canvas.width / 2, this.$canvas.height * (5 / 100));
+
+					// Bottom Text
+					context.strokeText(bottomText, this.$canvas.width / 2, this.$canvas.height * (90 / 100));
+					context.fillText(bottomText, this.$canvas.width / 2, this.$canvas.height * (90 / 100));
+
+					this.resizeCanvas(this.$canvas.height, this.$canvas.width)
 				}
 				image.src = reader.result;
 
 			};
 
 			reader.readAsDataURL(this.$imageInput.files[0]);
+		}
+	}
+
+	downloadMeme() {
+		if (!this.$imageInput.files[0]) {
+			this.$imageInput.parentElement.classList.add('has-error');
+			return;
+		}
+		if (this.$bottomTextInput.value === '') {
+			this.$imageInput.parentElement.classList.remove('has-error');
+			this.$bottomTextInput.parentElement.classList.add('has-error');
+			return;
+		}
+
+		this.$imageInput.parentElement.classList.remove('has-error');
+		this.$bottomTextInput.parentElement.classList.remove('has-error');
+
+		const imageSource = this.$canvas.toDataURL('image/jpg');
+		let att = document.createAttribute('href');
+		att.value = imageSource.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+		this.$downloadButton.setAttributeNode(att);
+	}
+
+	resizeCanvas(canvasHeight, canvasWidth) {
+		let height = canvasHeight;
+		let width = canvasWidth;
+		this.$canvas.style.height = `${height}px`;
+		this.$canvas.style.width = `${width}px`;
+
+		const minSize = 1000; 
+		while (height > Math.min(minSize, deviceWidth - 30) && width > Math.min(minSize, deviceWidth - 30)) {
+			height /= 2;
+			width /= 2;
+			this.$canvas.style.height = `${height}px`;
+			this.$canvas.style.width = `${width}px`;
 		}
 	}
 }
